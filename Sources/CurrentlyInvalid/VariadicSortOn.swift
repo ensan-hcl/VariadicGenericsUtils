a2d3f4ebb10dbbb8c94fdd244dd3555ed9d584b0
ensan <ensan.hcl.dev@gmail.com>
2023-06-06 22:59:47 +0900

    Add doc

--- Sources/CurrentlyInvalid/VariadicSortOn.swift
+++ Sources/CurrentlyInvalid/VariadicSortOn.swift
@@ -6,6 +6,12 @@
 //
 
 public extension Sequence {
+    /// Returns a sorted array of the sequence's elements using the given keypaths.
+    ///  ```swift
+    ///  let result = array.sorted(on: \.firstKey, \.secondKey, \.thirdKey)
+    ///  ```
+    /// - Parameter parameters: KeyPaths to sort on.
+    /// - Returns: A sorted array of the sequence's elements.
     func sorted<each T: Comparable>(on parameters: repeat KeyPath<Element, each T>) -> [Element] {
         func compareByKeyPath(left: Element, right: Element) -> Bool {
             // 2023-06-06
