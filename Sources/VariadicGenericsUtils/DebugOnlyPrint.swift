f0cf858e9e1c082d049877c785eca02cd838e918
ensan <ensan.hcl.dev@gmail.com>
2023-06-08 00:54:54 +0900

    Make changes

---
+++ Sources/VariadicGenericsUtils/DebugOnlyPrint.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by β α on 2023/06/08.
+//
+
+import Foundation
--- Sources/VariadicGenericsUtils/VariadicLessThan.swift
+++ Sources/VariadicGenericsUtils/VariadicLessThan.swift
@@ -5,15 +5,15 @@
 //  Created by ensan on 2023/06/06.
 //
 
-/// Checks if the elements in the left sequence are greater than the corresponding elements in the right sequence.
+/// Checks if the elements in the left sequence are less than the corresponding elements in the right sequence.
 ///
 /// - Parameters:
 ///   - left: A sequence of elements of type `T`.
 ///   - right: A sequence of elements of type `T`.
 ///
-/// - Returns: `true` if the elements in the right sequence are greater or equal than the corresponding elements in the left sequence, and left and right is not totally equal.
+/// - Returns: `left < right`. If the elements in the right sequence are greater or equal than the corresponding elements in the left sequence, and left and right is not totally equal.
 
-@inlinable public func greater<each T: Comparable>(left: repeat each T, right: repeat each T) -> Bool {
+@inlinable public func lessThan<each T: Comparable>(left: repeat each T, right: repeat each T) -> Bool {
     var result: Bool? = nil
     func mutatingCompare<U: Comparable>(left: U, right: U, result: inout Bool?) {
         if result != nil {
